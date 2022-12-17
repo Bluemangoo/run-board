942f15ad763045ca2c04de3a25d482e7b8b019ca
Bluemangoo <chenfy2006@qq.com>
2022-12-17 20:10:42 +0800

    v1.3.2

--- src/components/TableRow.vue
+++ src/components/TableRow.vue
@@ -1,3 +1,4 @@
+<!--suppress CssUnusedSymbol -->
 <template>
   <tr :class="color">
     <td>{{ name }}</td>
@@ -15,12 +16,15 @@ export default {
     whole: Number,
   },
   data() {
-    return {color: "untargeted"}
+    return {color: "common"}
   },
   watch:{
     whole: {
       handler() {
-        if (this.whole >=40){
+        if (this.whole >=100){
+          this.color="gold";
+        }
+        else if (this.whole >=40){
           this.color="red";
         }
         else if(this.whole>=30){
@@ -54,6 +58,9 @@ tr {
 }
 .red{
   background-color: rgba(255, 0, 0, 0.5);
+}
+.gold{
+  background-color: rgba(255, 215, 0, 0.5);
 }
 td {
   border-right: solid 2px #afafaf;
